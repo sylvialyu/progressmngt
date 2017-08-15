2d5edd5fe5aa72c4d108230fd512bcd46b4107ca
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-08-15 18:07:28 +0800

    beautify course#index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u = User.create(email: "sylvia_lyw@126.com", password: "Jobjob44")
+u.save
+
+Course.create!(
+  id: 1,
+  title: "Book 1",
+)
