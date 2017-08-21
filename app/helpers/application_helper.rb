5bbeb9a43ff9a7a180fa89384e0d68281490eb97
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-08-21 12:51:53 +0800

    add user_name in index

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def render_user_name(user_id)
+    if user_id == 4
+      "Lisa"
+    elsif user_id == 5
+      "Diana"
+    else
+      "Sylvia"
+    end
+  end
+
 end
