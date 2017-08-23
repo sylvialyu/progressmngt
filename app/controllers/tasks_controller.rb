b29e8c3448d043f4cc3abecdba96338ec4cb007d
sylvia.lyu <hisylvia.lyu@gmail.com>
2017-08-23 12:35:42 +0800

    update tasks_controllers with friendly_id

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,12 +8,12 @@ class TasksController < ApplicationController
   end
 
   def new
-    @course = Course.find(params[:course_id])
+    @course = Course.find_by_friendly_id!(params[:course_id])
     @task = Task.new
   end
 
   def create
-    @course = Course.find(params[:course_id])
+    @course = Course.find_by_friendly_id!(params[:course_id])
     @task = Task.new(task_params)
     @task.course = @course
     @task.user = current_user
@@ -26,12 +26,12 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @course = Course.find(params[:course_id])
+    @course = Course.find_by_friendly_id!(params[:course_id])
     @task = Task.find(params[:id])
   end
 
   def update
-    @course = Course.find(params[:course_id])
+    @course = Course.find_by_friendly_id!(params[:course_id])
     @task = Task.find(params[:id])
     @task.course = @course
     @task.user = current_user
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @course = Course.find(params[:course_id])
+    @course = Course.find_by_friendly_id!(params[:course_id])
     @task = Task.find(params[:id])
     @task.destroy
     redirect_to course_path(@course)
@@ -52,7 +52,7 @@ class TasksController < ApplicationController
 
 
   def finish
-    @course = Course.find(params[:course_id])
+    @course = Course.find_by_friendly_id!(params[:course_id])
     @task = Task.find(params[:id])
     @task.course = @course
     @task.finish!
@@ -60,7 +60,7 @@ class TasksController < ApplicationController
   end
 
   def unfinish
-    @course = Course.find(params[:course_id])
+    @course = Course.find_by_friendly_id!(params[:course_id])
     @task = Task.find(params[:id])
     @task.course = @course
     @task.unfinish!
@@ -71,7 +71,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:content, :completed)
+    params.require(:task).permit(:content, :completed, :friendly_id)
   end
 
 
